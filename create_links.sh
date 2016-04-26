c141aff918dd2a123ad381fa20f3d21cfcbc8873
Yury Bilenkis <yury.bilenkis@ostrovok.ru>
2016-04-26 11:10:11 +0300

    new create_links

--- create_links.sh
+++ create_links.sh
@@ -7,11 +7,13 @@ bak=`find conf/ -type f | sed s";^conf/;$HOME/;g"`
 for i in $bak; do
     mv $i{,.bak}
 done
+# vim backup
 mv $HOME/.vim{,.bak}
+mkdir -p $HOME/.vim/autoload
 
+# create links to new config
 for i in `find conf/ -type f`; do
      src=`pwd`/$i
      dst=`echo "$i" | sed s";^conf/;$HOME/;g"`
      ln -s $src $dst
 done
-ln -s `pwd`/conf/.vim $HOME/.vim
