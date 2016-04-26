b4bfd4d2dc2a8de94b07cb8cb0a20d2a6aa589e6
Yury Bilenkis <yury.bilenkis@ostrovok.ru>
2016-04-26 10:50:02 +0300

    + right config for .vim

--- create_links.sh
+++ create_links.sh
@@ -7,9 +7,11 @@ bak=`find conf/ -type f | sed s";^conf/;$HOME/;g"`
 for i in $bak; do
     mv $i{,.bak}
 done
+mv $HOME/.vim{,.bak}
 
 for i in `find conf/ -type f`; do
      src=`pwd`/$i
      dst=`echo "$i" | sed s";^conf/;$HOME/;g"`
      ln -s $src $dst
 done
+ln -s `pwd`/conf/.vim $HOME/.vim
