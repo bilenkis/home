fc9394395e866d4da70e272aa22064df085a210d
Yury Bilenkis <bil9178@gmail.com>
2017-07-13 23:40:47 +0300

    rm legacy

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 PATH=/usr/local/bin:/usr/bin:/bin
 
-aptitude update -qq && aptitude upgrade -qq
-aptitude install -qq \
-    vim \
-    zsh \
-    openbox \
-    tint2 \
-    keepassx
-
-sh copy_configs.sh
+apt-get update -y && apt-get upgrade  -y
+apt-get install -y \
+	ansible
