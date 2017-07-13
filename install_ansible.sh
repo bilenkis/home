e468964de3b68b3ece26968b3c251772e93599f0
Yury Bilenkis <bil9178@gmail.com>
2017-07-14 01:56:00 +0300

    update install_ansible; right grml zsh customize with .zshrc.local

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-PATH=/usr/local/bin:/usr/bin:/bin
-
-apt-get update -y && apt-get upgrade  -y
-apt-get install -y \
-	ansible
+apt-get update
+apt-get install -y python-pip python-setuptools libssl-dev python-dev
+pip install --upgrade pip
+pip install --upgrade ansible=="2.2.1.0"
