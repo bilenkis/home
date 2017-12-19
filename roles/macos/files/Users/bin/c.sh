2fa16d3c43358bac6d0736bc4c1e28c05a3fa44d
Yury Bilenkis <bil9178@gmail.com>
2017-12-19 18:48:07 +0300

    fix c.sh for hosts without tmux

--- roles/macos/files/Users/bin/c.sh
+++ roles/macos/files/Users/bin/c.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+if [[ -z $1 ]]; then
+	echo "Usage: $0 hostname"
+	exit 1
+fi
 mosh $1 -- sh -c "tmux a || tmux new"
 if [[ $? != "0" ]]; then 
-	ssh -t $1 "tmux a || tmux new"
+	ssh -t $1 "tmux a || tmux new" || ssh -t $1
 fi
