af95235976b15420836bd04b2d5844d98b34ee91
Yury Bilenkis <bil9178@gmail.com>
2018-08-06 18:55:29 +0300

    + mitogen

--- roles/macos/files/Users/bin/c.sh
+++ roles/macos/files/Users/bin/c.sh
@@ -3,7 +3,7 @@ if [[ -z $1 ]]; then
 	echo "Usage: $0 hostname"
 	exit 1
 fi
-mosh $1 -- sh -c "tmux a || tmux new"
+mosh $1 -- sh -c "tmux a -d || tmux new"
 if [[ $? != "0" ]]; then 
-	ssh -t $1 "tmux a || tmux new" || ssh -t $1
+	ssh -t $1 "tmux a -d || tmux new" || ssh -t $1
 fi
