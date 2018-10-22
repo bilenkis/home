0fef350c0000c81e2c2e92a78479e10688c98bf7
Yury Bilenkis <bil9178@gmail.com>
2018-10-22 18:58:35 +0300

    some refactoring

--- install_ansible_macos.sh
+++ install_ansible_macos.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-curl -sLo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-sudo -H python /tmp/get-pip.py
-sudo -H pip install --upgrade ansible=="2.2.1.0"
+set -xe
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+curl -Lo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+python /tmp/get-pip.py
 rm /tmp/get-pip.py
-
+pip install --upgrade ansible=="2.6.4"
+git submodule init
