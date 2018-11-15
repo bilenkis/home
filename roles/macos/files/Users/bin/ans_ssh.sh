5bdcc078030bb8e4cfbd8c077c19adf2be32edc0
Yuri Bilenkis <yuri.bilenkis@factorymarket.com>
2018-11-15 15:26:11 +0300

    update ans_ssh.sh

--- roles/macos/files/Users/bin/ans_ssh.sh
+++ roles/macos/files/Users/bin/ans_ssh.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-grep ansible_host ~/src/hosts \
+if [[ -z $1 ]]; then
+    echo "Usage: $0 path/to/hosts"
+    exit 1
+fi
+
+grep ansible_host $1 \
   | grep -v ';' \
   | awk '{print "host", $1, "\nhostname", $2, "\n"}' \
-  | sed 's/ansible_host=//' > ~/.ssh/config.d/additional_hosts
+  | sed 's/ansible_host=//' > ~/.ssh/config.d/ansible_hosts
