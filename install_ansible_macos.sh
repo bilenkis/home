135e71bacb0f60fae4cf97e41a27e851d796f73d
Yuri Bilenkis <yuri.bilenkis@factorymarket.com>
2019-01-28 23:28:06 +0300

    mv necessary pip packages to dotfiles role

--- install_ansible_macos.sh
+++ install_ansible_macos.sh
@@ -2,10 +2,13 @@
 set -xe
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python
+brew install python3 virtualenv
 curl -Lo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-python /tmp/get-pip.py
+python3 /tmp/get-pip.py
 rm /tmp/get-pip.py
-pip install --upgrade ansible=="2.6.4"
+python3 -m venv ~/.venv/main
+source ~/.venv/main/bin/activate
+pip3 install --upgrade pip3
+pip3 install -r requirements.txt
 git submodule init
 git submodule update
