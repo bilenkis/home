eb3f40bc57425a8330a4e6124cabdf1b798db51e
Yuri Bilenkis <yuri.bilenkis@syncplicity.com>
2020-05-13 23:04:24 +0300

    use requirements.txt

--- install_ansible_macos.sh
+++ install_ansible_macos.sh
@@ -2,12 +2,12 @@
 set -xe
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python3 virtualenv
+brew install python3 virtualenv pyenv
 curl -Lo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
 python3 /tmp/get-pip.py
 rm /tmp/get-pip.py
-python3 -m venv ~/.venv/main
-source ~/.venv/main/bin/activate
+pyenv virtualenv 3.7.5 main
+pyenv activate main
 pip3 install --upgrade pip3
 pip3 install -r requirements.txt
 git submodule update
